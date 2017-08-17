f1d8e7e3f21b2a0d4498e0cf1d61a29624e476c7
Erik McComb <erik@erikmccomb.com>
2017-08-17 13:22:06 -0600

    added files needed for app. set app structure

---
+++ 8ball.rb
@@ -0,0 +1,36 @@
+def answers
+  [
+    "Absolutely!",
+    "It is decidedly so",
+    "Of Course!",
+    "Yes definitely",
+    "Count on it, daddio",
+    "I mean, I think so",
+    "Probably",
+    "The cylons told me yes",
+    "Yes",
+    "Signs point to yes",
+    "Clairvoyance out of order; try again.",
+    "Magic 8 ball sleepy. ask me again later",
+    "I have no clue, dude",
+    "I cant see the future until I catch up on game of thrones. Ask later",
+    "See, tou say you wanna know, but I dont think you wanna know. Ask harder.",
+    "Not a chance, fool",
+    "No",
+    "No means no",
+    "It's not looking good for you, dude",
+    "Probably not"
+  ]
+end
+
+def ask_or_quit
+end
+
+def Rand
+end
+
+def question
+end
+
+def shake
+end
