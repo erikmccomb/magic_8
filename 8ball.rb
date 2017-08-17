8cf4a3219874a2c1a868a86fac2573e40e987c43
Erik McComb <erik@erikmccomb.com>
2017-08-17 14:23:51 -0600

    working 8 ball

--- 8ball.rb
+++ 8ball.rb
@@ -1,36 +1,52 @@
-def answers
-  [
-    "Absolutely!",
-    "It is decidedly so",
-    "Of Course!",
-    "Yes definitely",
-    "Count on it, daddio",
-    "I mean, I think so",
-    "Probably",
-    "The cylons told me yes",
-    "Yes",
-    "Signs point to yes",
-    "Clairvoyance out of order; try again.",
-    "Magic 8 ball sleepy. ask me again later",
-    "I have no clue, dude",
-    "I cant see the future until I catch up on game of thrones. Ask later",
-    "See, tou say you wanna know, but I dont think you wanna know. Ask harder.",
-    "Not a chance, fool",
-    "No",
-    "No means no",
-    "It's not looking good for you, dude",
-    "Probably not"
-  ]
+require 'pry'
+require 'colorize'
+
+def ask
+  sel = ""
+  puts ""
+  puts "Ask me a question, puny flesh bag, and you shall know your future!"
+  puts "Or, I guess type Exit if you're leaving"
+  puts ""
+  sel = gets.strip.downcase
+  case sel
+    when sel == "exit"
+      puts "Fine. Leave. I dont care... I miss you."
+    else  
+      answers =[
+        "42",
+        "Absolutely!",
+        "It is decidedly so",
+        "Of Course!",
+        "Yes definitely",
+        "Count on it, daddio",
+        "I mean, I think so",
+        "Probably",
+        "The cylons told me yes",
+        "Yes",
+        "Signs point to yes",
+        "Clairvoyance out of order; try again.",
+        "Magic 8 ball sleepy. ask me again later",
+        "I have no clue, dude",
+        "I cant see the future until I catch up on Game of Thrones. Ask later",
+        "See, tou say you wanna know, but I dont think you wanna know. Ask harder.",
+        "Not a chance, fool",
+        "No",
+        "No means no",
+        "It's not looking good for you, dude",
+        "Probably not"
+      ]
+      puts ''
+      puts 'You asked:'
+      puts sel
+      puts ''
+      puts 'BEHOLD MY WISDOM, MORTAL!'
+      puts answers.sample
+    end
+  ask
 end
 
-def ask_or_quit
-end
-
-def Rand
-end
-
-def question
-end
-
-def shake
-end
+puts ""
+puts "BEHOLD"
+puts "My coming was foretold by Mattel."
+puts "I am the all knowing, all powerful, sometimes lazy Magic 8 ball!"
+ask
